331fe496a650cc1e731295b7de3c5f2db6582072
shawnlknight <shawn.leberknight@gmail.com>
2017-03-21 17:23:28 -0400

    Updated heading font sizes

--- src/components/info.js
+++ src/components/info.js
@@ -4,9 +4,9 @@ export default () => (
   <section className="cf w-100 pv3 f6 ph3 ph4-ns">
     <div className="mw9 center ph3-ns f4">
       <div className="fl w-100 pa2 tc">
-        <h2>
+        <h3 className="f2-ns lh-copy">
           We are a group of people seeking truth and authentic friendships.
-        </h2>
+        </h3>
         <p>
           Here all voices are equally important. If you want to find out more, please <a href="mailto:joeysvendsenbc@gmail.com" className="link underline-hover red">reach out!</a>
         </p>
