806f7bf40c8c0e35e77cc00aed1b997c87a0607b
shawnlknight <shawn.leberknight@gmail.com>
2017-03-22 14:15:09 -0400

    added icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import Icon from './icon'
+
 export default () => (
   <footer className="cf w-100 pv3 f6 ph3 ph4-ns tr">
-    <h4 className="mid-gray">Charleston, SC</h4>
+    <a href="https://www.facebook.com/housechurchincharleston/?ref=bookmarks" target="_blank"><Icon icon="facebook" style="blue" width="30" height="30" /></a>
+    <h4 className="mid-gray">Made with <Icon icon="heart" style="red" width="14" height="14" /> in Charleston, SC</h4>
   </footer>
 )
---
+++ src/components/icon.js
@@ -0,0 +1,31 @@
+import React from 'react'
+const {PropTypes} = React
+
+const icons = {
+  facebook: 'M608 192h160v-192h-160c-123.514 0-224 100.486-224 224v96h-128v192h128v512h192v-512h160l32-192h-192v-96c0-17.346 14.654-32 32-32z',
+  heart: 'M755.188 64c-107.63 0-200.258 87.554-243.164 179-42.938-91.444-135.578-179-243.216-179-148.382 0-268.808 120.44-268.808 268.832 0 301.846 304.5 380.994 512.022 679.418 196.154-296.576 511.978-387.206 511.978-679.418 0-148.392-120.43-268.832-268.812-268.832z'
+}
+
+const styles = {
+  blue: {
+    fill: "#3b5998"
+  },
+  red: {
+    fill: "#e7040f"
+  }
+}
+
+const Icon = props => (
+  <svg width={props.width} height={props.height} viewBox="0 0 1024 1024">
+    <path style={styles[props.style]} d={icons[props.icon]}></path>
+  </svg>
+)
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired
+};
+
+export default Icon
